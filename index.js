7017b051dad53ec1ca5cf340e757ad50f8324d7c
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-02-27 19:21:21 -0500

    created full skeleton of the Express Install including json package dependencies

--- index.js
+++ index.js
@@ -23,21 +23,10 @@ app.get('/', (req, res) => {
 app.use(cookieParser())
 app.use(bodyParser())
 
-//reference to passport.js
-require('./config/passport')(passport)
-app.use(passport.initialize())
-app.use(passport.session())
 app.use(flash())
-//reference to user login information to create a session
-app.use(function (req, res, next) {
-    res.locals.currentUser = req.userName
-    next()
-})
-app.use(session({secret: 'req.userName'}))
 
 app.use(methodOverride("_method"));
 app.use('/enter the location youd like this to be from', controllerController)
-app.use('/user', userController)
 
 //sets up the localhost and shows what nodemon should say if its working correctly
 app.set("port", process.env.PORT || 3001);
